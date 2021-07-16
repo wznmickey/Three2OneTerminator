2b15fc7f88c826a2e5c97eb3d28cca2c0205a141
wznmickey <wznmickey@sjtu.edu.cn>
2021-07-16 21:43:06 +0800

    nameConflict

---
+++ src/LoadMod.elm
@@ -0,0 +1,19 @@
+module LoadMod exposing (loadMod)
+
+import Area exposing (..)
+import GameData exposing (..)
+import Json.Decode exposing (..)
+
+
+loadMod : String -> ( GameData, String )
+loadMod st =
+    let
+        ans =
+            decodeString dGameData st
+    in
+    case ans of
+        Ok x ->
+            ( x, "Ok" )
+
+        Err x ->
+            ( initGameData, Json.Decode.errorToString x )
