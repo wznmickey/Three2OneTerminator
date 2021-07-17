9a10a50177eac937f1a262eb07d539b5e081487e
wznmickey <wznmickey@sjtu.edu.cn>
2021-07-17 23:45:45 +0800

    updateCPwithTime

---
+++ src/For.elm
@@ -0,0 +1,31 @@
+module For exposing (..)
+
+{--
+    First Int (`i`) is to check how the for process
+    Second Int (`n`) set the times of function `f`
+    Type outer (`o`) is the outer environment data
+    ( outer -> Int->  io -> (io,outer)) set a function which use (outer environment data) & (for function process) to map a function from type io to type io
+    the left is used for the above function
+--}
+
+
+for_outer : Int -> Int -> (outer -> Int -> io -> ( io, outer )) -> ( io, outer ) -> ( io, outer )
+for_outer i n f ( x, o ) =
+    if i >= n then
+        ( x, o )
+
+    else
+        for_outer (i + 1) n f (f o i x)
+
+
+
+{--No outer var for function--}
+
+
+for_no_outer : Int -> Int -> (Int -> io -> io) -> io -> io
+for_no_outer i n f x =
+    if i >= n then
+        x
+
+    else
+        for_no_outer (i + 1) n f (f i x)
--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,27 @@
 module Main exposing (main)
 
 import Area exposing (..)
+import Array exposing (..)
 import Browser exposing (element)
 import Browser.Events exposing (onAnimationFrameDelta, onClick)
+import CPdata exposing (..)
+import CPtype exposing (CPtype(..))
+import CRdata exposing (CRdata)
 import Dict exposing (Dict)
-import GameData exposing (GameData, initGameData,getPureCPdataByName)
+import For exposing (..)
+import GameData exposing (GameData, getPureCPdataByName, initGameData)
 import HelpText exposing (initHelpText)
 import Html exposing (..)
 import Html.Attributes as HtmlAttr exposing (..)
 import Http
 import LoadMod exposing (loadMod)
 import Msg exposing (Msg(..), State(..))
+import PureCPdata exposing (PureCPdata)
 import String exposing (..)
 import Svg exposing (Svg)
 import Svg.Attributes as SvgAttr
 import View exposing (..)
-import CRdata exposing (CRdata)
-import PureCPdata exposing (PureCPdata)
-import CPdata exposing(..)
+
 
 type alias Model =
     { data : GameData
@@ -25,12 +29,13 @@ type alias Model =
     , modInfo : String
     , loadInfo : String
     , onviewArea : String
+    , time : Float
     }
 
 
 initModel : Model
 initModel =
-    Model initGameData Start "modInfo" "Init" "init"
+    Model initGameData Start "modInfo" "Init" "init" 0
 
 
 init : () -> ( Model, Cmd Msg )
@@ -77,13 +82,23 @@ update msg model =
         Clickon areaNum ->
             ( { model | onviewArea = areaNum }, Cmd.none )
 
+        Tick time ->
+            let
+                newmodel1 =
+                    { model | time = model.time + time }
+
+                newmodel2 =
+                    { model | data = updateData model.data }
+            in
+            ( newmodel2, Cmd.none )
+
         _ ->
             ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch []
+    Sub.batch [ onAnimationFrameDelta Tick ]
 
 
 main =
@@ -94,23 +109,59 @@ main =
         , subscriptions = subscriptions
         }
 
-effectCP :  Dict String PureCPdata -> Dict String PureCPdata -> Dict String PureCPdata -> (Dict String PureCPdata,Dict String PureCPdata)
-effectCP  effect global local =
-    (dictEffectCP effect global,dictEffectCP effect global)
+
+effectCP : Dict String PureCPdata -> Dict String PureCPdata -> Dict String PureCPdata -> ( Dict String PureCPdata, Dict String PureCPdata )
+effectCP effect global local =
+    ( dictEffectCP effect global, dictEffectCP effect local )
 
 
 dictEffectCP : Dict String PureCPdata -> Dict String PureCPdata -> Dict String PureCPdata
 dictEffectCP effect before =
     Dict.map (getEffect effect) before
 
-getEffect : Dict String PureCPdata-> String -> PureCPdata -> PureCPdata
+
+getEffect : Dict String PureCPdata -> String -> PureCPdata -> PureCPdata
 getEffect effect key before =
-    if (before.name==key) then
-        valueEffectCP (getPureCPdataByName (key,effect)) before 
-    else 
+    if before.name == key then
+        valueEffectCP (getPureCPdataByName ( key, effect )) before
+
+    else
         before
+
 
 valueEffectCP : PureCPdata -> PureCPdata -> PureCPdata
 valueEffectCP effect before =
-    {before|data=before.data+effect.data}
+    { before | data = before.data + effect.data }
 
+
+updateData : GameData -> GameData
+updateData data =
+    let
+        ( newArea, newGlobal ) =
+            areaCPchange data.area data.globalCP
+    in
+    { data | area = newArea, globalCP = newGlobal }
+
+
+areaCPchange : Dict String Area -> Dict String PureCPdata -> ( Dict String Area, Dict String PureCPdata )
+areaCPchange area global =
+    let
+        pureArea =
+            Array.map (\( x, y ) -> y) (Array.fromList (Dict.toList area))
+
+        ( afterAreaArray, afterGlobal ) =
+            for_outer 0 (Dict.size area) eachAreaCPchange ( pureArea, global )
+    in
+    ( Dict.fromList (List.map (\x -> ( x.name, x )) (Array.toList afterAreaArray)), afterGlobal )
+
+
+eachAreaCPchange : Dict String PureCPdata -> Int -> Array Area -> ( Array Area, Dict String PureCPdata )
+eachAreaCPchange global i a =
+    let
+        newArea =
+            Maybe.withDefault initArea (Array.get i a)
+
+        ( newGlobal, local ) =
+            effectCP newArea.effect global newArea.localCP
+    in
+    ( Array.map ((\y x -> { x | localCP = y }) local) a, newGlobal )
