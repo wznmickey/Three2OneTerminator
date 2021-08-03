48485b0b14267ef5e640eb3b3cd597ca2138a506
wznmickey <wznmickey@sjtu.edu.cn>
2021-08-03 19:12:57 +0800

    HelpText docs

--- src/HelpText.elm
+++ src/HelpText.elm
@@ -1,17 +1,36 @@
-module HelpText exposing (..)
+module HelpText exposing (HelpText,initHelpText,decoder_HelpText,encodeHelpText)
+{-| It is the module that defines type `HelpText` used as a subtype and related functions.
+
+
+# Type
+
+@docs HelpText
+
+# Init
+
+@docs initHelpText
+
+
+# Json relate
+
+@docs decoder_HelpText, encodeHelpText
+
+-}
 
 import Dict exposing (Dict)
-import Json.Decode exposing (..)
-import Json.Encode
+import Json.Decode exposing (Decoder,map,dict,string)
+import Json.Encode exposing (Value,string)
 
-
+{-| This type defines the help text of area, CP and CR.
+-}
 type alias HelpText =
     { name : String
     , -- Help text.
       text : String
     }
 
-
+{-| This function gives a init `HelpText`, should not be actually used in `Running`.
+-}
 initHelpText : HelpText
 initHelpText =
     { name =
@@ -20,6 +39,8 @@ initHelpText =
         "init"
     }
 
+{-| This function decodes `HelpText`.
+-}
 
 decoder_HelpText : Decoder (Dict.Dict String HelpText)
 decoder_HelpText =
@@ -32,9 +53,11 @@ decoder_HelpText =
             )
         )
         (dict
-            string
+            Json.Decode.string
         )
 
+{-| This function encodes `HelpText`.
+-}
 
 encodeHelpText : HelpText -> Json.Encode.Value
 encodeHelpText data =
